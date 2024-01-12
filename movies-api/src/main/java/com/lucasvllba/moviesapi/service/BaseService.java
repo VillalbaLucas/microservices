2ccb882e86c49cd3b94524a906dea93455a90a53
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-01-12 15:54:51 -0300

    add: findByTitle and findByGender

--- movies-api/src/main/java/com/lucasvllba/moviesapi/service/BaseService.java
+++ movies-api/src/main/java/com/lucasvllba/moviesapi/service/BaseService.java
@@ -3,10 +3,13 @@ package com.lucasvllba.moviesapi.service;
 import java.util.List;
 
 import com.lucasvllba.moviesapi.model.Cinematography;
+import com.lucasvllba.moviesapi.model.enums.CineGender;
 
 public interface BaseService<E extends Cinematography, ID> {
     public void save(E entity);
     public void deleteById(ID id);
     public List<E> findAll();
     public E findById(ID id);
+    public List<E> findByGender(CineGender gender);
+    public List<E> findByTitle(String title);
 }
