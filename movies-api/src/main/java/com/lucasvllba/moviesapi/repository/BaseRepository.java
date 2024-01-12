aa93004d4211555b46ebb80dde08ce3df235b70c
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-01-12 15:53:49 -0300

    refact: move findByTitle and finByGender to BaseRepository

--- movies-api/src/main/java/com/lucasvllba/moviesapi/repository/BaseRepository.java
+++ movies-api/src/main/java/com/lucasvllba/moviesapi/repository/BaseRepository.java
@@ -1,9 +1,14 @@
 package com.lucasvllba.moviesapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import com.lucasvllba.moviesapi.model.enums.CineGender;
+
 @NoRepositoryBean
 public interface BaseRepository<E, ID> extends JpaRepository<E , ID>{
-    
+    public List<E> findByGender(CineGender gender);
+    public List<E> findByTitle(String title);
 }
