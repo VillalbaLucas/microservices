fb7459c83a37f76741d36c9c21f57343ca28301a
VillalbaLucas <lucasvllba.develop@gmail.com>
2024-01-12 15:40:21 -0300

    create: Series repository

--- movies-api/src/main/java/com/lucasvllba/moviesapi/repository/SeriesRepository.java
+++ movies-api/src/main/java/com/lucasvllba/moviesapi/repository/SeriesRepository.java
@@ -1,9 +1,14 @@
 package com.lucasvllba.moviesapi.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lucasvllba.moviesapi.model.Series;
+import com.lucasvllba.moviesapi.model.enums.CineGender;
 
 @Repository
 public interface SeriesRepository extends BaseRepository<Series, Long> {
+    public List<Series> findByGender(CineGender gender);
+    public List<Series> findByTitle(String title);
 }
