e3c7e23d15184e49afa643ec3e8d09e39ca0a885
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-06 20:24:54 -0300

    add: movieClient and create: findAllMovies

--- persons-api/src/main/java/com/lucasvllba/personsapi/service/ActorService.java
+++ persons-api/src/main/java/com/lucasvllba/personsapi/service/ActorService.java
@@ -1,14 +1,26 @@
 package com.lucasvllba.personsapi.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.lucasvllba.personsapi.client.MovieClient;
+import com.lucasvllba.personsapi.dto.MovieDto;
 import com.lucasvllba.personsapi.model.Actor;
 import com.lucasvllba.personsapi.repository.BaseRepository;
 
 @Service
 public class ActorService extends BaseService<Actor, Long> {
 
-    public ActorService(BaseRepository<Actor, Long> baseRepo) {
+    private BaseRepository<Actor, Long> baseRepo;
+    private MovieClient movieClient;
+
+    public ActorService(BaseRepository<Actor, Long> baseRepo, MovieClient movieClient) {
         super(baseRepo);
+        this.movieClient = movieClient;
+    }
+
+    public List<MovieDto> findAllMovies(Long id) {
+        return movieClient.getAllByActorId(id);
     }
 }
