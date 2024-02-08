42f8d126c7ab037fe07e1dbdf8ac1b0c07e389a6
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-08 14:05:42 -0300

    create: findAllSeries in service

--- persons-api/src/main/java/com/lucasvllba/personsapi/service/ActorService.java
+++ persons-api/src/main/java/com/lucasvllba/personsapi/service/ActorService.java
@@ -5,22 +5,29 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.lucasvllba.personsapi.client.MovieClient;
+import com.lucasvllba.personsapi.client.SeriesClient;
 import com.lucasvllba.personsapi.dto.MovieDto;
+import com.lucasvllba.personsapi.dto.SeriesDto;
 import com.lucasvllba.personsapi.model.Actor;
 import com.lucasvllba.personsapi.repository.BaseRepository;
 
 @Service
 public class ActorService extends BaseService<Actor, Long> {
 
-    private BaseRepository<Actor, Long> baseRepo;
     private MovieClient movieClient;
+    private SeriesClient seriesClient;
 
-    public ActorService(BaseRepository<Actor, Long> baseRepo, MovieClient movieClient) {
+    public ActorService(BaseRepository<Actor, Long> baseRepo, MovieClient movieClient, SeriesClient seriesClient) {
         super(baseRepo);
         this.movieClient = movieClient;
+        this.seriesClient=seriesClient;
     }
 
     public List<MovieDto> findAllMovies(Long id) {
         return movieClient.getAllByActorId(id);
+    }
+
+    public List<SeriesDto> findAllSeries(Long id) {
+        return seriesClient.getAllByActorId(id);
     }
 }
