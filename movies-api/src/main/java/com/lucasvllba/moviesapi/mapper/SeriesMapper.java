f0812989d1c5213dd99bc7293641633027f97ef0
VillalbaLucas <lukasvillalba2016@gmail.com>
2024-02-08 15:48:57 -0300

    fix: mapper seriers

--- movies-api/src/main/java/com/lucasvllba/moviesapi/mapper/SeriesMapper.java
+++ movies-api/src/main/java/com/lucasvllba/moviesapi/mapper/SeriesMapper.java
@@ -4,9 +4,17 @@ import com.lucasvllba.moviesapi.model.Series;
 
 public class SeriesMapper {
     public static void setAll(Series data, Series modifiedSeries){
-        data.setGender(modifiedSeries.getGender());
-        data.setPremiereDate(modifiedSeries.getPremiereDate());
-        data.setTitle(modifiedSeries.getTitle());
-        data.setSeasons(modifiedSeries.getSeasons());
+        if(modifiedSeries.getGender() != null)
+            data.setGender(modifiedSeries.getGender());
+        if(modifiedSeries.getPremiereDate() != null)
+            data.setPremiereDate(modifiedSeries.getPremiereDate());
+        if(modifiedSeries.getTitle() != null)
+            data.setTitle(modifiedSeries.getTitle());
+        if(modifiedSeries.getSeasons() != 0)
+            data.setSeasons(modifiedSeries.getSeasons());
+        if(modifiedSeries.getActorsId() != null)
+            data.setActorsId(modifiedSeries.getActorsId());
+        if(modifiedSeries.getDirectorId() != null)
+            data.setDirectorId(modifiedSeries.getDirectorId());
     }
 }
